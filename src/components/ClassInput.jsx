eca709941f7dfbbae1306540887c9dd8d400ebd8
Suman Das <sum4ndas@gmail.com>
2023-11-24 21:45:38 +0530

    handleEditClick() successfully edits and save resubmit

--- src/components/ClassInput.jsx
+++ src/components/ClassInput.jsx
@@ -54,8 +54,52 @@ class ClassInput extends Component {
     console.log(e.target.textContent);
     if (e.target.textContent == "Edit") {
       e.target.textContent = "Resubmit";
+
+      let itemToEdit = e.target.parentNode.firstChild.textContent;
+
+      // create new array where the target's editing value will be true
+      let newArray = this.state.todos.map((todo) => {
+        if (todo.taskName == itemToEdit) {
+          return { taskName: todo.taskName, editing: true };
+        } else {
+          return todo;
+        }
+      });
+      // console.log(newArray);
+
+      // rerender new todos
+      this.setState((state) => ({
+        ...state,
+        todos: newArray,
+      }));
     } else {
       e.target.textContent = "Edit";
+
+      // Find the item to resubmit with id because it will not change during input
+      let itemToResubmit = e.target.parentNode.firstChild.id;
+      // console.log(itemToResubmit);
+
+      // create new array with modified task
+      let newArray = this.state.todos.map((todo) => {
+        // we compare the taskName with itemToResubmit found through ID.
+        // if we used firstChild.value to find taskName, it would not work because
+        // input text will differ from taskName. So we set ID same as taskName in the <input> jsx.
+        if (todo.taskName == itemToResubmit) {
+          return {
+            taskName: e.target.parentNode.firstChild.value,
+            editing: false,
+          };
+        } else {
+          return todo;
+        }
+      });
+
+      // rerender edited todos
+      this.setState((state) => ({
+        ...state,
+        todos: newArray,
+      }));
+      // console.log(todos);
     }
   }
 
