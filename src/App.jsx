a6c73520a788bc24020d2eb99443baa7364108d9
Suman Das <sum4ndas@gmail.com>
2023-12-13 08:10:40 +0530

    Install react-router-dom and adding a router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// Use this 'Link' element instead of regular 'a' tag to stop browser from reloading.
 
 const App = () => {
-  const [heading, setHeading] = useState("Magnificent Monkeys");
-
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
-
   return (
-    <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some example of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
