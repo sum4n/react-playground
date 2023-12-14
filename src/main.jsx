bef7d7ac6c1fe579a808e2aad9c6ccbb3db5ec0c
Suman Das <sum4ndas@gmail.com>
2023-12-14 07:32:20 +0530

    Refactoring the routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
-import Profile from "./components/Profile.jsx";
-import ErrorPage from "./components/ErrorPage.jsx";
+import Router from "./components/Router";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import App from "./App.jsx";
+// import Profile from "./components/Profile.jsx";
+// import ErrorPage from "./components/ErrorPage.jsx";
 // import Spinach from "./components/Spinach.jsx";
 // import Popeye from "./components/Popeye.jsx";
 // import DefaultProfile from "./components/DefaultProfile.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "profile/:name",
-    element: <Profile />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//   },
+//   {
+//     path: "profile/:name",
+//     element: <Profile />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>
 );
